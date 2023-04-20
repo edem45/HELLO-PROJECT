6e48fe0720306bab0367639042a0796501457cd2
Edem K <kimalidinov@list.ru>
2023-04-20 17:54:09 +0300

    Example007 Magic

---
+++ Example007-Magic/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Console.Clear();
+
+int xa = 8;
+int ya = 1;
+int xb = 1;
+int yb = 6;
+int xc = 16; 
+int yc = 6;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+int x = xa, y = yb;
+
+int count = 0;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+
+if (what == 0)
+{
+    x = (x + xa) / 2;
+    y = (y + ya) / 2;
+}
+if (what == 1)
+{
+    x = (x + xb) / 2;
+    y = (y + yb) / 2;
+}
+if (what == 2)
+{
+    x = (x + xc) / 2;
+    y = (y + yc) / 2;
+}
+   Console.SetCursorPosition(x, y);
+   Console.WriteLine("+");
+   count++;
+}
